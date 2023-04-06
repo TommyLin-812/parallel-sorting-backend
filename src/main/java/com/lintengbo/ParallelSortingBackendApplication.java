eb56a946560ae0ea6d11d07d3ec0f422a027d00b
TommyLin <1065770912@qq.com>
2023-04-06 14:37:19 +0800

    Initial Commit

---
+++ src/main/java/com/lintengbo/ParallelSortingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lintengbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelSortingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelSortingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lintengbo/ParallelSortingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lintengbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelSortingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
